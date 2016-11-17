f3ce7bd213203736f86feec3fcd7bb40ff122418
ilumer <18888106831qxq@gmail.com>
2016-11-17 23:13:54 +0800

    补上自己的app.java

--- app/src/main/java/com/fast/ilumer/gank/App.java
+++ app/src/main/java/com/fast/ilumer/gank/App.java
@@ -1,8 +1,26 @@
 package com.fast.ilumer.gank;
 
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.stetho.Stetho;
+
 /**
  * Created by root on 11/17/16.
  */
 
-public class App {
+public class App extends Application {
+
+    public static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        context = getApplicationContext();
+    }
+
+    public static Context getAppContext(){
+        return App.context;
+    }
 }
